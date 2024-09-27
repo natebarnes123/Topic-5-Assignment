c48495bca3dc6ca69e297b6c5173fd382ad1fbe8
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-27 09:39:40 -0400

    hgcnbvvnb

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double busPass, busPass2;
+            busPass = 2.00;
+            busPass2 = 3.50;
+            int age;+
+            Console.WriteLine("Hello there, how old are you?");
+            double.TryParse(Console.ReadLine(), out busPass);
+
+            if (age >= 12 && age <= 60)
+            {
+                Console.WriteLine("Your buss pass will be " + busPass);
+            }
+            else 
+            {
+                Console.WriteLine("You bus pass will cost you" + busPass2);
+            }
+
+
+
         }
     }
 }
