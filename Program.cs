da33c21390da9db65b57cccfd913000cab0ad8d2
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-01 09:41:37 -0400

    fdgdgfbbgfdbgf

--- Program.cs
+++ Program.cs
@@ -16,11 +16,12 @@
             Console.WriteLine("I have information for the following planets");
             Console.WriteLine("1. Venus  2. Mars  3. Jupiter  4. Saturn  5. Uranus  6. Neptune");
             Console.WriteLine(" Which planet are you visiting");
-            planet = Console.ReadLine();
+            planet = Console.ReadLine().ToLower();
 
 
-            if (planet == "venus" earthWeight * 0.7)
+            if (planet == "1" || planet == "venus")
             {
+                spaceWeight = earthWeight * 0.7;
                 Console.WriteLine("Your weight on venus is" + spaceWeight);
             }
 
