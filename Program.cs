a61e80a2c99566850e68d8d2505279b5043a3060
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-01 09:38:02 -0400

    gfhfcdhxsgzehfcgtrjyvhrfhcgdexhc

--- Program.cs
+++ Program.cs
@@ -2,23 +2,29 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args, double earthWeight)
         {
-            double busPass, busPass2;
-            busPass = 2.00;
-            busPass2 = 3.50;
-            int age;+
-            Console.WriteLine("Hello there, how old are you?");
-            double.TryParse(Console.ReadLine(), out busPass);
+            //Topic 5 assingment question 1
 
-            if (age >= 12 && age <= 60)
+           
+            
+            double earthWeight;
+            double spaceWeight;
+            string planet;
+            Console.WriteLine("Please enter your current earth weight.");
+            earthWeight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("I have information for the following planets");
+            Console.WriteLine("1. Venus  2. Mars  3. Jupiter  4. Saturn  5. Uranus  6. Neptune");
+            Console.WriteLine(" Which planet are you visiting");
+            planet = Console.ReadLine();
+
+
+            if (planet == "venus" earthWeight * 0.7)
             {
-                Console.WriteLine("Your buss pass will be " + busPass);
+                Console.WriteLine("Your weight on venus is" + spaceWeight);
             }
-            else 
-            {
-                Console.WriteLine("You bus pass will cost you" + busPass2);
-            }
+
+
 
 
 
